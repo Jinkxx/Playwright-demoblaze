f1c9235f89e51c19e902fbbc2301148ce7a445e4
Jinkxx <everythinginreturninfo.com>
2026-06-29 23:51:06 -0400

    beforeeach and first test

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -6,3 +6,15 @@ const { test, expect } = require('@playwright/test');
  * Tests all items on the homepage
 */
 
+test.describe('Homepage Tests', () => {
+
+    test.beforeEach(async ({ page }) => {
+        // Go to the homepage before each test
+        await page.goto('https://www.demoblaze.com/');
+    });
+    
+    test('Check homepage title', async ({ page }) => {
+        // Expect a title to be "STORE".
+        await expect(page).toHaveTitle('STORE');
+    });
+});
