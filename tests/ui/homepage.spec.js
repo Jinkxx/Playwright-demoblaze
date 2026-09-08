a53b6cde003d6032f72afcb617c18084025c0f9d
Jinkxx <everythinginreturninfo.com>
2026-09-08 12:38:08 -0400

    refactorer homepage with tests only using page objects

--- tests/ui/homepage.spec.js
+++ tests/ui/homepage.spec.js
@@ -20,4 +20,20 @@ test.describe('Homepage Tests', () => {
     test('check homepage title', async () => {
         await expect(homePage.page).toHaveTitle('STORE');
     });
+
+    test('check homepage logo', async () => {
+        await expect(homePage.logo).toBeVisible();
+    });
+
+    test('check homepage categories', async () => {
+        await expect(homePage.categories).toHaveCount(4);
+    });
+
+    test('check homepage carousel', async () => {
+        await expect(homePage.carousel).toBeVisible();
+    });
+
+    test('check homepage footer', async () => {
+        await expect(homePage.footer).toBeVisible();
+    });
 });
